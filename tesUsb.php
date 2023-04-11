498a19161c9dcea6ed582671d136976146e8e9f8
elektadepo <elektadepo@gmail.com>
2023-04-12 00:19:14 +0700

    okusb

--- tesUsb.php
+++ tesUsb.php
@@ -2,10 +2,13 @@
 exec("mode COM3 BAUD=9600 PARITY=N data=8 stop=1 xon=off");
 
 $fp = fopen ("COM3", "w");
+fwrite($fp, "\r");
+fwrite($fp, "buka4\n");
+fclose($fp);
 if (!$fp) {
    echo "Not open";
 } else {
    echo "Open";
-   fwrite($fp, "1");
-    fclose($fp);
 }
+
+?>
