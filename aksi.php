41b31ff361a7285bb327437eaa53c539a77de83d
elektadepo <elektadepo@gmail.com>
2023-04-14 23:06:30 +0700

    ok

--- aksi.php
+++ aksi.php
@@ -6,7 +6,7 @@ exec("mode COM5 BAUD=9600 PARITY=N data=8 stop=1 xon=off");
 $fp = fopen ("com5", "w");
 sleep(2);
 fwrite($fp, "\r");
-fwrite($fp, "buka\n");
+fwrite($fp, "tutup\n");
 fclose($fp);
 if (!$fp) {
    // echo "Not open";
--- prosesambil.php
+++ prosesambil.php
@@ -41,7 +41,7 @@ exec("mode COM5 BAUD=9600 PARITY=N data=8 stop=1 xon=off");
 $fp = fopen ("com5", "w");
 sleep(2);
 fwrite($fp, "\r");
-fwrite($fp, "buka\n");
+fwrite($fp, "buka".$room."\n");
 fclose($fp);
 if (!$fp) {
    // echo "Not open";
