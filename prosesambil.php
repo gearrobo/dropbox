454731985a3cc310723554c6c3a0f9e251181149
elektadepo <elektadepo@gmail.com>
2023-04-14 23:01:27 +0700

    ok

--- prosesambil.php
+++ prosesambil.php
@@ -35,18 +35,19 @@ $sql = " UPDATE docu SET sender = 'a',
       echo "ERROR, tidak berhasil". mysqli_error($conn);
     }
 
-// exec("mode ".$com." BAUD=9600 PARITY=N data=8 stop=1 xon=off");
+exec("mode COM5 BAUD=9600 PARITY=N data=8 stop=1 xon=off");
 
 
-// $fp = fopen ($com, "w");
-// fwrite($fp, "\r");
-// fwrite($fp, "buka".$room."\n");
-// fclose($fp);
-// if (!$fp) {
-//    // echo "Not open";
-// } else {
-//    // echo "Open";
-// }
+$fp = fopen ("com5", "w");
+sleep(2);
+fwrite($fp, "\r");
+fwrite($fp, "buka\n");
+fclose($fp);
+if (!$fp) {
+   // echo "Not open";
+} else {
+   // echo "Open";
+}
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
