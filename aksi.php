6ee264217ebac30566295825fbfb38c5162b5da1
gatot nugroho <gearroboone@gmail.com>
2023-04-14 23:00:08 +0700

    ok

--- aksi.php
+++ aksi.php
@@ -1,17 +1,18 @@
 <?php 
 
-// exec("mode ".$com." BAUD=9600 PARITY=N data=8 stop=1 xon=off");
+exec("mode COM5 BAUD=9600 PARITY=N data=8 stop=1 xon=off");
 
 
-// $fp = fopen ($com, "w");
-// fwrite($fp, "\r");
-// fwrite($fp, "buka".$room."\n");
-// fclose($fp);
-// if (!$fp) {
-//    // echo "Not open";
-// } else {
-//    // echo "Open";
-// }
+$fp = fopen ("com5", "w");
+sleep(2);
+fwrite($fp, "\r");
+fwrite($fp, "buka\n");
+fclose($fp);
+if (!$fp) {
+   // echo "Not open";
+} else {
+   // echo "Open";
+}
 
 
 header('location:index.php');
