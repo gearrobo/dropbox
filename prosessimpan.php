b630e6330b4515cbb1b2438f9c327aea44c9ac11
gatot nugroho <gearroboone@gmail.com>
2023-04-17 11:35:38 +0700

    ok

--- prosessimpan.php
+++ prosessimpan.php
@@ -61,6 +61,62 @@ if(isset($_POST['simpan'])){
                  Data Tanaman Berhasil di Tambah!
              </div>
          ';
+         $apiToken = "5065160162:AAEVWCHA-TEiqZq7eKQagiTy5pa-Op-myVI";
+$string = 'Dokumen Tersimpan dengan Token '.$token;
+$data = [
+   'chat_id' => '1201127925',
+   'text' => $string
+];
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" .
+                           http_build_query($data) );
+
+
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
+
+// //Load Composer's autoloader
+// require 'vendor/autoload.php';
+
+// //Create an instance; passing `true` enables exceptions
+// $mail = new PHPMailer(true);
+
+// try {
+//     //Server settings
+//     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+//     $mail->isSMTP();                                            //Send using SMTP
+//     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+//     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//     $mail->Username   = '@gmail.com';                     //SMTP username
+//     $mail->Password   = 'jyylgvenvthnvkqt';                               //SMTP password
+//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+//     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+//     //Recipients
+//     $mail->setFrom('@gmail.com', 'Mailer');
+//    //  $mail->addAddress('budisantosokavling@gmail.com', 'Joe User');     //Add a recipient
+//     $mail->addAddress('androexe5@gmail.com', 'Resky');     //Add a recipient
+//     //$mail->addAddress('ellen@example.com');               //Name is optional
+//     //$mail->addReplyTo('info@example.com', 'Information');
+//     //$mail->addCC('cc@example.com');
+//     //$mail->addBCC('bcc@example.com');
+
+//     //Attachments
+//     //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//     //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+//     //Content
+//     $mail->isHTML(true);                                  //Set email format to HTML
+//     $mail->Subject = 'Dropbox Automation System Test';
+//     $mail->Body    = 'Dokumen Anda Telah diambil dengan token '.$token.' <b>in bold!</b>';
+//     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+//     $mail->send();
+//     echo 'Message has been sent';
+// } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
+
          header('location:jedasimpan.php');
      }else{
          echo "ERROR, tidak berhasil". mysqli_error($conn);
