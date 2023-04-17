dac769f4af2e1e30a6dab10f45a20d6b0f5c2825
gatot nugroho <gearroboone@gmail.com>
2023-04-17 11:32:08 +0700

    ok

--- prosesambil.php
+++ prosesambil.php
@@ -49,6 +49,17 @@ if (!$fp) {
    // echo "Open";
 }
 
+
+$apiToken = "5065160162:AAEVWCHA-TEiqZq7eKQagiTy5pa-Op-myVI";
+$string = '<b>bold</b>, <strong>bold</strong>';
+$data = [
+   'chat_id' => '1201127925',
+   'text' => $string
+];
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" .
+                           http_build_query($data) );
+
+
 // use PHPMailer\PHPMailer\PHPMailer;
 // use PHPMailer\PHPMailer\SMTP;
 // use PHPMailer\PHPMailer\Exception;
